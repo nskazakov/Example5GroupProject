921ac2c914860c3aee61cfad47aa69bc0f392e16
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-09-23 19:03:58 +0200

    Initial Commit

---
+++ 5retailProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5retailProject
+//
+//  Created by Nikita Kazakov on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
