c31c0b86a450a4b678b2aa5780def91faaa8b940
Nikita Kazakov <nikita.kazakov95@gmail.com>
2021-10-16 15:39:13 +0200

    Create and implement HeaderViewObject.

---
+++ 5retailProject/Sources/Scenes/Profile/Views/Models/HeaderViewObject.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderViewObject.swift
+//  5retailProject
+//
+//  Created by Nikita Kazakov on 16.10.2021.
+//
+
+import Foundation
+
+struct HeaderViewObject: Equatable {
+    let title: String
+    let code: String
+}
